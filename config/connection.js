2603d64401a3c94a860d02b196c6bb7bb89bed8d
wilcook <wil.cook.is@gmail.com>
2017-01-21 14:09:38 -0500

    credentials uploaded

---
+++ config/connection.js
@@ -0,0 +1,34 @@
+// *********************************************************************************
+// CONNECTION.JS - THIS FILE INITIATES THE CONNECTION TO MYSQL
+// *********************************************************************************
+
+// Dependencies
+var Sequelize = require("sequelize");
+
+// Selects a connection (can be changed quickly as needed)
+
+// Creates mySQL connection using Sequelize
+var sequelize = new Sequelize(process.env.JAWSDB_URL,
+{
+  dialect: 'mysql',
+
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  }
+
+});
+
+sequelize.authenticate()
+    .then(function () {
+        console.log("CONNECTED!");
+    })
+    .catch(function (err) {
+        console.log(err);
+    })
+    .done();
+
+
+// Exports the connection for other files to use
+module.exports = sequelize;

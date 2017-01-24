4a8c8d79fd338175a93c2e3d854e49745747e40c
Fausto Brito <G30@Faustos-MacBook-Air.local>
2017-01-24 13:33:41 -0500

    fixing demo

---
+++ public/demo.js
@@ -0,0 +1,23 @@
+
+var http = require('http');
+var bodyParser = require('body-parser');
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: 'wgmw3rs78pvwk4e.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user: 'x5xhg901hjefvq0u',
+    password: 'ixcckomsi3ojkt7a'
+});
+
+connection.connect(function(err) {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
+    console.log('connected as id ' + connection.threadId);
+});
+
+
+    connection.query('INSERT INTO contacts (uid, cname, phone) VALUES (1, ' + cname + ', ' + cphone + ')', function(error, results, fields) {
+    	console.log(err);
+
+    });

3bed10ebc8b0c7d1f10c087fcd40230c77961848
wil.cook.is@gmail.com <wil.cook.is@gmail.com>
2017-02-22 16:23:39 -0500

    revert for demo

--- app.js
+++ app.js
@@ -2,33 +2,12 @@ var http = require('http');
 var fs = require('fs');
 // var formidable = require("formidable");
 var util = require('util');
-var mysql = require('mysql');
 
 var express = require('express');
 var path = require('path');
-var ejs = require('ejs');
 
 var app = express(); 
 var PORT = process.env.PORT || 8080;
-
-/// mysql connection for demo ///
-var connection = mysql.createConnection({
-  host: 'mwgmw3rs78pvwk4e.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-  user: 'x5xhg901hjefvq0u',
-  password: 'ixcckomsi3ojkt7a',
-  database: 'iv28dtvanxwxdm51'
-});
-
-connection.connect(function (err) {
-  if (err) {
-    console.error('error connecting: ' + err.stack);
-    return;
-  }
-  console.log('connected as id ' + connection.threadId);
-});
-
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
 
 app.use(express.static(__dirname + '/public'));
 
@@ -45,8 +24,13 @@ app.get('/vitals', function(req, res) {
 
 });
 
-
 app.get('/contacts', function(req, res){
+  res.render('anotherpage', {
+    title: 'Home'
+  });
+});
+
+app.get('/contacts/:id', function(req, res){
   res.render('anotherpage', {
     title: 'Home'
   });
@@ -58,31 +42,13 @@ app.get('/about', function(req, res){
   });
 });
 
-// app.get('/contacts/:id', function(req, res){
-//   res.render('anotherpage', {
-//     title: 'Home'
-//   });
-// });
-
-// app.post('/contacts', function (req, res){
-//   connection.query('INSERT INTO contacts (uid, cname, phone) VALUES (?, ?, ?)', [1, req.body.magicName, req.body.magicNumber], function (err, result) {
-//     if (err) throw (err);
-//     // res.redirect('./about');
-//     console.log(req.body.magicName);
-//     console.log(req.body.magicNumber);
-//   });
-// });
+app.post('/contacts', function (req, res) {
+  var cname = req.body.magicName
+  var cphone = req.body.magicNumber
+})
 
 
-
-// app.post('/contacts', function (req, res) {
-//   var cname = req.body.magicName;
-//   var cphone = req.body.magicNumber;
-//   res.send()
-// })
-
-
+// app.listen(3000);
 
 
 app.listen(PORT);
-
